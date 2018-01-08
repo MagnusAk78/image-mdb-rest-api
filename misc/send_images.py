8c2ec9ea88495ef3f4ee030d70625df366e70314
MagnusAk78 <magnus.akerman@chalmers.se>
2018-01-08 14:11:15 +0000

    Update for convenience

---
+++ misc/send_images.py
@@ -0,0 +1,24 @@
+import picamera
+from time import sleep
+import base64
+import requests
+import json
+
+def convertImageToBase64():
+  with open("image_test.jpg", "rb") as image_file:
+    encoded = base64.b64encode(image_file.read())
+    return encoded
+
+camera = picamera.PiCamera()
+camera.resolution = (640,480)
+
+while True:
+  sleep(5)
+  camera.capture('image.jpg')
+  
+  image_64 = base64.b64encode(open("image.jpg","rb").read())
+  
+  payload = json.dumps({'base64' : image_64 })
+  
+  headers = {'Content-Type' : 'application/json'}
+  r = requests.post("http://192.168.1.196:8080/image/insert", headers = headers, data = payload)
