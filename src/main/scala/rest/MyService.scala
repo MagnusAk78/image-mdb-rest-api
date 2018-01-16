c87a3b7862df0eec290366df249ba98740121968
MagnusAk78 <magnusaakerman78@gmail.com>
2018-01-16 21:10:41 +0100

    bugfix path - pathprefix nonsens

--- src/main/scala/rest/MyService.scala
+++ src/main/scala/rest/MyService.scala
@@ -44,7 +44,7 @@ trait MyService extends HttpService {
     import spray.httpx.SprayJsonSupport.sprayJsonUnmarshaller
 
     pathPrefix(Segment) { originName =>
-      path("image") {
+      pathPrefix("image") {
         path(LongNumber) { timestamp =>
           get {
             respondWithMediaType(`application/json`) {
@@ -104,7 +104,7 @@ trait MyService extends HttpService {
           }
         }
       } ~
-      path("images") {
+      pathPrefix("images") {
       	path("count") {
       		get {
       			respondWithMediaType(`application/json`) {
@@ -135,78 +135,78 @@ trait MyService extends HttpService {
       			}
       		}
       	}
-      } ~
-      pathPrefix("aggregation") {
-      	path("status") {
-      		get {
-      			respondWithMediaType(`application/json`) {
-      				complete {
-      					val futures = aggregationClients.map { _ ? AskStatusMessage }
-      					futures.map { future => Await.result(future, timeout.duration).asInstanceOf[AggretationStatus] }
-      				}
-      			}
-      		}
-      	}
-      } ~
-      path("") {
-      	get {
-      		respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
-      			complete {
-      				"""
-      				<html>
-      				<head>
-      				<title>Image MongoDB REST API</title>
-      				</head>
-      				<body>
-      				<h2>Image MongoDB REST API</h2>
-  
-      				<h4>Get image from a specific origin with specific timestamp</h4>
-      				<code>GET:  /&lt;originName&gt;/image/&lt;timestamp&gt;</code><br>
-  
-      				<h4>Get latest inserted image from a specific origin</h4>
-      				<code>GET:  &lt;originName&gt;/image/latest</code> <br>
-  
-      				<h4>Get oldest inserted image from a specific origin</h4>
-      				<code>GET:  /&lt;originName&gt;/image/oldest</code> <br>
-  
-      				<h4>Insert new image from a specific origin</h4>
-      				<code>POST: /&lt;originName&gt;/image/insert</code> <br>
-  
-      				<code><pre>
-      				{
-      				"base64": Image data, &lt;Base64String&gt;
-      				}
-      				</pre></code>
-      				<p>Upload an image from a specific origin as an base64 encoded string.</p>
-  
-      				<h4>Count current number of images stored from a specific origin</h4>
-      				<code>GET:  /&lt;originName&gt;/images/count</code> <br>
-  
-      				<h4>Query images</h4>
-      				<code>POST: /&lt;originName&gt;/images/query</code> <br>
-  
-      				<code><pre>
-      				{
-      				"fromTimestamp": &lt;timestamp&gt;,
-      				"toTimestamp": &lt;timestamp&gt;,
-      				"limit": &lt;limit&gt;
-      				}
-      				</pre></code>
-      				<p>Assign 0 to any field to ignore it.</p> 
-      				<br>
-  
-      				<h4>Get status of eventual aggregation</h4>
-      				<code>GET: /aggregation/status</code> <br>
-  
-      				</pre></code>
-      				<p>Assign 0 or empty to any field to ignore it.</p>    				    
-      				</body>
-      				</html>
-      				"""
-      			}
-      		}
-      	}
       }
+    } ~
+    pathPrefix("aggregation") {
+    	path("status") {
+    		get {
+    			respondWithMediaType(`application/json`) {
+    				complete {
+    					val futures = aggregationClients.map { _ ? AskStatusMessage }
+    					futures.map { future => Await.result(future, timeout.duration).asInstanceOf[AggretationStatus] }
+    				}
+    			}
+    		}
+    	}
+    } ~
+    path("") {
+    	get {
+    		respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+    			complete {
+    				"""
+    				<html>
+    				<head>
+    				<title>Image MongoDB REST API</title>
+    				</head>
+    				<body>
+    				<h2>Image MongoDB REST API</h2>
+
+    				<h4>Get image from a specific origin with specific timestamp</h4>
+    				<code>GET:  /&lt;originName&gt;/image/&lt;timestamp&gt;</code><br>
+
+    				<h4>Get latest inserted image from a specific origin</h4>
+    				<code>GET:  &lt;originName&gt;/image/latest</code> <br>
+
+    				<h4>Get oldest inserted image from a specific origin</h4>
+    				<code>GET:  /&lt;originName&gt;/image/oldest</code> <br>
+
+    				<h4>Insert new image from a specific origin</h4>
+    				<code>POST: /&lt;originName&gt;/image/insert</code> <br>
+
+    				<code><pre>
+    				{
+    				"base64": Image data, &lt;Base64String&gt;
+    				}
+    				</pre></code>
+    				<p>Upload an image from a specific origin as an base64 encoded string.</p>
+
+    				<h4>Count current number of images stored from a specific origin</h4>
+    				<code>GET:  /&lt;originName&gt;/images/count</code> <br>
+
+    				<h4>Query images</h4>
+    				<code>POST: /&lt;originName&gt;/images/query</code> <br>
+
+    				<code><pre>
+    				{
+    				"fromTimestamp": &lt;timestamp&gt;,
+    				"toTimestamp": &lt;timestamp&gt;,
+    				"limit": &lt;limit&gt;
+    				}
+    				</pre></code>
+    				<p>Assign 0 to any field to ignore it.</p> 
+    				<br>
+
+    				<h4>Get status of eventual aggregation</h4>
+    				<code>GET: /aggregation/status</code> <br>
+
+    				</pre></code>
+    				<p>Assign 0 or empty to any field to ignore it.</p>    				    
+    				</body>
+    				</html>
+    				"""
+    			}
+    		}
+    	}
     }
   }
 }
